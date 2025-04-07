857f8017f60ce986853abcf5d871bb54364dc0a5
Andrew <andrijvasilik252@gmail.com>
2025-04-07 12:54:06 +0300

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,20 @@
-import React from 'react';
+import React, { useState, useRef, useEffect } from 'react';
+export const App: React.FC = () => {
+  const [key, setKey] = useState<string>('');
+  const divRef = useRef<HTMLDivElement>(null);
 
-export const App: React.FC = () => (
-  <div className="App">
-    <p className="App__message">The last pressed key is [Enter]</p>
-  </div>
-);
+  useEffect(() => {
+    divRef.current?.focus();
+  }, []);
+  const handleKeyDown = (event: React.KeyboardEvent): void => {
+    setKey(event.key);
+  };
+
+  return (
+    <div className="App" ref={divRef} onKeyDown={handleKeyDown} tabIndex={0}>
+      {' '}
+      {!key && <p className="App__message">Nothing was pressed yet</p>}
+      {key && <p className="App__message">The last pressed key is [{key}]</p>}
+    </div>
+  );
+};
