682c6857cc06d9ff2f7e5d6b93302e30e55e9506
Andrew <andrijvasilik252@gmail.com>
2025-04-07 13:05:39 +0300

    fix errors

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,37 @@
-import React, { useState, useRef, useEffect } from 'react';
-export const App: React.FC = () => {
-  const [key, setKey] = useState<string>('');
-  const divRef = useRef<HTMLDivElement>(null);
+import React from 'react';
 
-  useEffect(() => {
-    divRef.current?.focus();
-  }, []);
-  const handleKeyDown = (event: React.KeyboardEvent): void => {
-    setKey(event.key);
+interface AppState {
+  key: string;
+}
+
+export class App extends React.Component<{}, AppState> {
+  state: AppState = {
+    key: '',
   };
 
-  return (
-    <div className="App" ref={divRef} onKeyDown={handleKeyDown} tabIndex={0}>
-      {' '}
-      {!key && <p className="App__message">Nothing was pressed yet</p>}
-      {key && <p className="App__message">The last pressed key is [{key}]</p>}
-    </div>
-  );
-};
+  divRef: React.RefObject<HTMLDivElement> = React.createRef();
+
+  componentDidMount() {
+    this.divRef.current?.focus();
+  }
+
+  handleKeyDown = (event: React.KeyboardEvent<HTMLDivElement>) => {
+    this.setState({ key: event.key });
+  };
+
+  render() {
+    const { key } = this.state;
+
+    return (
+      <div
+        className="App"
+        ref={this.divRef}
+        onKeyDown={this.handleKeyDown}
+        tabIndex={0}
+      >
+        {!key && <p className="App__message">Nothing was pressed yet</p>}
+        {key && <p className="App__message">The last pressed key is [{key}]</p>}
+      </div>
+    );
+  }
+}
